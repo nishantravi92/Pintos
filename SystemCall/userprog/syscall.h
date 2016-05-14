a5e1a15b3ef1eeed5b4ce7325c18bb7b58d799a7
nishantravi92 <nishantravi92@gmail.com>
2016-05-14 16:16:53 -0400

    System Call Handler added

--- SystemCall/userprog/syscall.h
+++
@@ -1,8 +0,0 @@
-#ifndef USERPROG_SYSCALL_H
-#define USERPROG_SYSCALL_H
-#define SYS_ERROR -1
-void syscall_init (void);
-int open(const char *);
-struct file *get_file(int);
-void sys_exit(int);
-#endif /* userprog/syscall.h */
